fd8ec8b6a44bb5df3e9366b2b0e63268eedfda56
Scott Ashley <burnbright987@gmail.com>
2022-06-20 14:17:29 -0400

    added User model and routes

---
+++ models/User.js
@@ -0,0 +1,50 @@
+const { INTEGER } = require('sequelize');
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our user model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // TABLE COLUMN DEFINITIONS GO HERE
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // this is the equivalent of SQL's `NOT NULL` option
+            allowNull: false,
+            // instruct that this is the Primary Key
+            primaryKey: true,
+            // turn on auto increment
+            autoIncrement: true
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        }
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        modelName: 'user'
+    },
+);
+
+module.exports = User;
